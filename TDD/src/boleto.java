94a214aa34cae30c2c7ad8fccbdad7401b24ce31
MarioFBSantos <71677017+MarioFBSantos@users.noreply.github.com>
2022-04-17 22:18:17 -0300

    Update boleto.java

--- TDD/src/boleto.java
+++ TDD/src/boleto.java
@@ -1,1 +1,43 @@
 
+package qet;
+import java.util.ArrayList;
+
+public class boleto {
+
+    private double codigo;
+    private String data;
+    private double pago;
+    
+    public boleto(double codigo, String data, double pago){
+      super();
+      this.data = data;
+      this.codigo = codigo;
+      this.pago = pago;
+    }
+    
+    public String getData(){
+        return data;
+    }
+    
+    public double getPago(){
+        return pago;
+    }
+    
+    public double getCodigo(){
+        return codigo;
+    }
+    
+    public void setData(String data){
+        this.data = data;
+    }
+    
+    public void setPago(double pago){
+        this.pago = pago;
+    }
+        
+    public void setCodigo(double codigo){
+        this.codigo = codigo;
+    }
+    
+}
+
