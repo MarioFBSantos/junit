719411f2f9c62b3dc041cc6ea8556745efeb3f77
MarioFBSantos <71677017+MarioFBSantos@users.noreply.github.com>
2022-04-17 22:20:06 -0300

    Create faturaTest.java

---
+++ TDD/test/qet/faturaTest.java
@@ -0,0 +1,27 @@
+package qet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+public class faturaTest {
+    
+    fatura fatura1;
+    
+    @Before
+    public void inicializa(){
+        fatura1 = new fatura(2000.0, "20/10/2022", "Jorge", false);
+    }
+    
+    @Test
+    public void testCriaFatura(){
+       assertTrue(2000.0 == fatura1.getTotal());
+       assertEquals("20/10/2022", fatura1.getData());
+       assertEquals("Jorge", fatura1.getNome());
+       assertEquals(false, fatura1.getPaga());
+    }
+    
+    
+}
