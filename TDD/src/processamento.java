7e7308627cfe66fcfa6166db2144ccbd832bc9b9
MarioFBSantos <71677017+MarioFBSantos@users.noreply.github.com>
2022-04-24 21:30:52 -0300

    interação 1 - teste quantidade boletos

--- TDD/src/processamento.java
+++ TDD/src/processamento.java
@@ -12,34 +12,8 @@ public class processamento {
         pagamento = new ArrayList();
     }
     
-    public double getTotalPago(){
-        double total = 0.0;
-        
-        for(Iterator i = pagamento.iterator(); i.hasNext();){
-             boleto item = (boleto) i.next();
-              total += item.getPago();
-        }
-        
-        return total;
-    }
-    
-    public void addBoleto(boleto item){
-        pagamento.add(item);
-        System.out.println(item);
-    }
-    
     public int qtdBoletos(){
         return pagamento.size();
-    }
-    
-    public void esvazia(){
-        pagamento.clear();
-    }
-    
-    public void setFaturaStatus(fatura item){
-        if(this.getTotalPago() >= item.getTotal()){
-            item.setPaga(true);
-        }
     }
     
 }
